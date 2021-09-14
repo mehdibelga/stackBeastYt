3a97a48c88afb575f0f34a94dfc7c595d34315be
mehdibelga <90707125+mehdibelga@users.noreply.github.com>
2021-09-14 15:57:34 +0100

    Update whatsaAppBot.js

--- whatsaAppBot.js
+++ whatsaAppBot.js
@@ -4,15 +4,12 @@ async function scrape(url) {
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(url);
-  await page.waitForSelector("span [title='Ek Villian']");
-  const target = await page.$("span [title='Ek Villian']");
+  await page.waitForSelector("span [title='Lwalida']");
+  const target = await page.$("span [title='Lwalida']");
   await target.click();
-  const inp = await page.$(
-    "#main > footer > div._3ee1T._1LkpH.copyable-area > div._3uMse > div > div._3FRCZ.copyable-text.selectable-text"
-  );
-
-  for (let i = 0; i < 100; i++) {
-    await inp.type("ok this is magic");
+  const inp = await page.$(<div role="textbox" class="_13NKt copyable-text selectable-text" contenteditable="true" data-tab="9" dir="ltr" spellcheck="true"></div>);
+  for (let i = 0; i < 50; i++) {
+    await inp.type("salam mama");
     await page.keyboard.press("Enter");
   }
 }
